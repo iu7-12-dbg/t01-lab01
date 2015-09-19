2e6f22e14962e9bf1506b613047fcd128f0120f1
nitrocaster <paul-kv@yandex.ru>
2015-09-20 02:40:59 +0300

    Add solution with stub projects.

---
+++ src/AStarDemo/Root.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace AStarDemo
+{
+    internal static class Root
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+        }
+    }
+}
