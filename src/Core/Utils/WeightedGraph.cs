ea3da1b242ef35286a8c8125f0965a97477923b2
Reaper38 <spiritgame38@gmail.com>
2015-12-10 00:40:47 +0300

    Graph implementation

---
+++ src/Core/Utils/WeightedGraph.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace Core.Utils
+{
+    public interface WeightedGraph<L>
+    {
+        int Cost(Location a, Location b);
+        IEnumerable<Location> Neighbors(Location id);
+    }
+
+    public struct Location
+    {
+        public readonly int x, y;
+
+        public Location(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
