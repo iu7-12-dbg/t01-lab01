e4e988506b623e057e42f4b75cc17740930c3aec
nitrocaster <paul-kv@yandex.ru>
2015-12-15 23:46:59 +0300

    Add base 2D object class.

---
+++ src/AStarDemo/SceneObject.cs
@@ -0,0 +1,17 @@
+﻿namespace AStarDemo
+{
+    using System.Drawing;
+    using Core.Math;
+
+    internal abstract class SceneObject
+    {
+        public int ZOrder;
+        public Color Color;
+        public bool Visible = true;
+        public bool Selected;
+        public int GroupId;
+
+        public abstract void Draw(Graphics g);
+        public abstract Box2 GetBBox();
+    }
+}
