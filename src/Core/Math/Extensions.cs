3250c045e2f0b6c5b29579c761965f493ba11096
nitrocaster <paul-kv@yandex.ru>
2015-12-17 16:52:33 +0300

    Core: add Clamp extension methods for double and float.

---
+++ src/Core/Math/Extensions.cs
@@ -0,0 +1,25 @@
+﻿namespace Core.Math
+{
+    public static class Extensions
+    {
+        public static double Clamp(this double self, double min, double max)
+        {
+            if (self<min)
+                return min;
+            else if (self>max)
+                return max;
+            else
+                return self;
+        }
+
+        public static float Clamp(this float self, float min, float max)
+        {
+            if (self<min)
+                return min;
+            else if (self>max)
+                return max;
+            else
+                return self;
+        }
+    }
+}
