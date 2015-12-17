bfb3e775a8cb6869584ac24f1cc0a66230683088
nitrocaster <paul-kv@yandex.ru>
2015-12-17 12:38:56 +0300

    Box2: add extra constructor, fix method access modifiers.

--- src/Core/Math/Box2.cs
+++ src/Core/Math/Box2.cs
@@ -6,11 +6,18 @@
     {
         public Vector2 Min;
         public Vector2 Max;
-
+        
         public Box2(Vector2 min, Vector2 max)
         {
             Min = min;
             Max = max;
+        }
+
+        public Box2(Vector2 center, double radius)
+        {
+            var r = new Vector2(radius, radius);
+            Min = center-r;
+            Max = center+r;
         }
 
         public void Set(Vector2 min, Vector2 max)
@@ -39,20 +46,22 @@
             get { return Max.Y; }
             set { Max.Y = value; }
         }
-        
-        bool Contains(double x, double y)
+
+        public bool Contains(double x, double y)
         { return Contains(new Vector2(x, y)); }
 
-        bool Contains(Vector2 v)
+        public bool Contains(Vector2 v)
         { return Min.X<=v.X && v.X<=Max.X && Min.Y<=v.Y && v.Y<=Max.Y; }
 
-        bool Contains(Box2 b)
+        public bool Contains(Box2 b)
         { return Contains(b.Min) && Contains(b.Max); }
 
-        void Include(Vector2 p)
+        public void Include(Vector2 p)
         {
             Min.Set(SysMath.Min(Min.X, p.X), SysMath.Min(Min.Y, p.Y));
             Max.Set(SysMath.Max(Max.X, p.X), SysMath.Max(Max.Y, p.Y));
         }
+
+        public static readonly Box2 Empty = new Box2(Vector2.MaxValue, Vector2.MinValue);
     }
 }
